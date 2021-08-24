a6410e507b79ef1881476948b2101ab0379b4105
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-08-24 10:12:58 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Join our channel', url="https://t.me/free_music123"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/free_music123'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://telegra.ph/file/21e06a011217f7308c3c9.jpg')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/song_requestgroup')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
