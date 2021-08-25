29e6dbade29bf7c7ba366a638c85f07122533d98
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-08-25 09:13:03 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -206,7 +206,7 @@ async def auto_filter(bot: Client, update: Message):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Check links at @geronimo1234 Found {(len_results)} Choose one you wanted: <code>{query}</code>",
+                text=f"Choose one you wanted:/n <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
