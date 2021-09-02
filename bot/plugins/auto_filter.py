27260db75cbbe339f455ed02414c84f8f1ed6bb6
geronimolover <84311253+geronimolover@users.noreply.github.com>
2021-09-02 11:22:35 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -206,7 +206,7 @@ async def auto_filter(bot: Client, update: Message):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Choose one you wanted:/n <code>{query}</code>",
+                text=f"Choose your book from below options:",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
